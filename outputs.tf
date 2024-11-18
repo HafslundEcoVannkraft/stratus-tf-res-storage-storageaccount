3f9fa1d4e5ab88b0d8c2fe356a3c706000a32140
Hein Tonny Køien <hein.tonny.koien@hafslund.no>
2024-11-18 14:57:07 +0000

    Add new variables and outputs to terraform module stratus-tf-res-storage-storageaccount

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "storage_account_resource_id" {
   value = azapi_resource.storage_account_v2.id
 }
+
+output "storage_account_name" {
+  value = azapi_resource.storage_account_v2.name
+}
--- variables.tf
+++ variables.tf
@@ -18,3 +18,15 @@ variable "pe_subnets" {
   type        = list(string)
   description = "List of subnets to create private endpoints for"
 }
+
+variable "container_names" {
+  type        = list(string)
+  description = "List of names for containers to create"
+  default     = []
+}
+
+variable "network_acls_bypass" {
+  type = string
+  description = "The network ACLs bypass value. Possible values are `AzureServices`, `None`, and `VirtualNetwork`."
+  default = "None"
+}
